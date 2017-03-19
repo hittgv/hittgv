bb98bf1238271a6dc627570a2ad369d98fc65eef
Nick McBlain <nickmcblain@gmail.com>
2017-03-19 11:42:53 +0000

    Loading spinner

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import queryService from '../api';
 
 import logo from '../assets/logo.svg';
+import grapes from '../assets/grapes.png';
 
 const getCurrentTabUrl = () => {
     const queryInfo = {
@@ -35,20 +36,26 @@ class App extends React.Component {
     constructor() {
         super();
         this.state = {
-            message: ''
+            message: '',
+            requestInFlight: false
         };
     }
 
     getText() {
         rating().then((rating) => {
             this.setState({
-                message: rating > 50 ? 'We don\'t have enough data to verify this webpage' : 'This news is likely to be fake'
+                message: rating > 50 ? 'We don\'t have enough data to verify this webpage' : 'This news is likely to be fake',
+                requestInFlight: false
             });
             console.log(this.state);
         });
     }
 
     componentDidMount() {
+        this.setState({
+            message: this.state.message,
+            requestInFlight: true
+        });
         this.getText();
     }
 
@@ -57,7 +64,11 @@ class App extends React.Component {
             <section>
                 <img className="logo" src={logo}/>
                 <h1>I heard it through the grape vine</h1>
-                <p>{this.state.message}</p>
+                {
+                    this.state.requestInFlight
+                    ? <img src={grapes} className="grapes" />
+                    : <p>{this.state.message}</p>
+                }
             </section>
         );
     }
